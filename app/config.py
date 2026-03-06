c4848b2bd71738e461fb5a108fb3db1c4d9db87a
Muhammad Qaim Ali <researchtycoons@gmail.com>
2026-03-07 03:44:37 +0500

    feat: Add config file with Settings to read from .env

---
+++ app/config.py
@@ -0,0 +1,11 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+        postgres_user: str
+        postgres_password: str
+        postgres_db: str
+
+        class Config:
+            env_file = ".env"
+
+settings = Settings()
